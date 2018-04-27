822b56220392e46d56973bb822a05939a1a0f766
djansyle <djansyledjans@gmail.com>
2018-04-27 10:17:07 +0800

    feat(verification): support send email verification and verifying link

--- email-service-client/index.js
+++ email-service-client/index.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 const assert = require('assert');
+const jwt = require('jsonwebtoken');
+const { URL } = require('url');
 
 const requiredFields = ['from', 'to', 'subject'];
 
@@ -7,20 +9,65 @@ function makeAsArray(arg) {
   return !(Array.isArray(arg)) ? [arg] : arg;
 }
 
-module.exports = (host, key) =>
-  async (param) => {
+function checkFields(param) {
+  requiredFields.forEach(field =>
+    assert(param[field], `Expected property \`${field}\` to have a value.`));
+}
+
+class EmailServiceClient {
+  constructor(host, key, region) {
+    this.host = host;
+    this.key = key;
+    this.region = region;
+  }
+
+  sendEmail(param) {
+    checkFields(param);
+
     const to = makeAsArray(param.to);
     const cc = makeAsArray(param.cc || []);
     const bcc = makeAsArray(param.bcc || []);
 
-    requiredFields.forEach(field => assert(`Expected property \`${field}\` to have a value.`));
-    assert(param.html || param.text, 'Expected either property `html` or `text` to be atleast present 1.');
-
-    return fetch(`${host}/email`, {
+    assert(param.html || param.text || param.template, 'Expected either property `html`, `text`, `template` to be atleast one present.');
+    return fetch(`${this.host}/email`, {
       method: 'POST',
-      headers: { key },
+      headers: { key: this.key, 'content-type': 'application/json' },
       body: JSON.stringify({
-        ...param, to, cc, bcc,
+        ...param, to, cc, bcc, region: this.region,
       }),
     });
-  };
+  }
+
+  async sendEmailVerification(param, secret) {
+    checkFields(param);
+    const key = jwt.sign({ ...param.payloadData, email: param.to }, secret);
+    const templateData = param.templateData || {};
+
+    return this.sendEmail({
+      ...param,
+      templateData: { ...templateData, key },
+    });
+  }
+
+  async verify(param, secret) {
+    if (typeof param !== 'string' && !param.request && !param.request.search) {
+      throw new Error('Parameter expected to be a `string` or `koa` request object.');
+    }
+
+    const href = typeof param === 'string' ? param : param.request.href;
+    const url = new URL(href);
+    const key = url.searchParams.get('key');
+
+    if (!key) {
+      return false;
+    }
+
+    try {
+      return jwt.verify(key, secret);
+    } catch (e) {
+      return false;
+    }
+  }
+}
+
+module.exports = EmailServiceClient;
