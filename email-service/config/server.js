c4e9df7b0f67fb2edd2bf2cbc0ea2f9fa65c940f
djansyle <djansyledjans@gmail.com>
2018-05-22 13:24:11 +0800

    fix(env): remove host env

--- email-service/config/server.js
+++ email-service/config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST,
   port: parseInt(process.env.PORT || '80', 10),
   secret: 'xozBn73n4xXtFYyA&j^x=W2uhBIMVwdxGF0q2GtEX^u@dQS1xLJvLRwSxMDPns7B',
   region: 'ap-southeast-1',
