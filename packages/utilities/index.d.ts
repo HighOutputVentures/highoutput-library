204404f6c6282430c027d402495327208c780baf
Roger Madjos <roger.madjos@gmail.com>
2018-10-22 09:16:31 +0800

    feat(logger): add silly log level

--- packages/utilities/index.d.ts
+++ packages/utilities/index.d.ts
@@ -2,9 +2,10 @@ declare module 'highoutput-utilities' {
   export class Logger {
     constructor(tags: string[]);
     tag(tag: string): Logger;
-    info(...args: any[]): void;
     error(...args: any[]): void;
-    verbose(...args: any[]): void;
     warn(...args: any[]): void;
+    info(...args: any[]): void;
+    verbose(...args: any[]): void;
+    silly(...args: any[]): void;
   }
 }
