89259f8c5f64063947cae760e9f682b55331ff85
Arjay A. Sitoy <sitoy.arjay@gmail.com>
2019-12-26 09:54:50 +0800

    test(logger): initial logger utility tests

---
+++ packages/logger/test/index.spec.ts
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import Logger from '../src';
+
+describe('logger', () => {
+  it('should accept strings', () => {
+    const logger = new Logger('test');
+
+    logger.log('log-level', 'Hello world!');
+    logger.info('Hello world!');
+    logger.error('Hello world!');
+    logger.warn('Hello world!');
+    logger.silly('Hello world!');
+    logger.verbose('Hello world!');
+
+    assert.ok(true);
+  });
+
+  it('should accept objects', () => {
+    const logger = new Logger('test');
+
+    logger.log('log-level', { message: 'Hello world!' });
+    logger.info({ message: 'Hello world!' });
+    logger.error({ message: 'Hello world!' });
+    logger.warn({ message: 'Hello world!' });
+    logger.silly({ message: 'Hello world!' });
+    logger.verbose({ message: 'Hello world!' });
+
+    assert.ok(true);
+  });
+
+  it('should accept Error instance', () => {
+    const logger = new Logger('test');
+
+    logger.log('log-level', new Error('Hello world!'));
+    logger.info(new Error('Hello world!'));
+    logger.error(new Error('Hello world!'));
+    logger.warn(new Error('Hello world!'));
+    logger.silly(new Error('Hello world!'));
+    logger.verbose(new Error('Hello world!'));
+
+    assert.ok(true);
+  });
+});
