b1e642602e97ef944a56c43238a825a787c18225
Roger Madjos <roger@highoutput.io>
2020-04-20 01:11:28 +0800

    feat: update EventStore client

--- packages/arque/src/lib/event-store/client.ts
+++ packages/arque/src/lib/event-store/client.ts
@@ -1,4 +1,9 @@
+import Amqp, { Subscriber, Client } from '@highoutput/amqp';
+import { EventEmitter } from 'events';
+import Backoff from 'backoff';
+import delay from '@highoutput/delay';
 import { Event, Snapshot, ID } from '../types';
+import { getAmqp } from '../util';
 
 export type EventStoreClient = {
   createEvent(params: {
@@ -29,4 +34,68 @@ export type EventStoreClient = {
     aggregateType?: string;
     type?: string;
   }, handler: (ack: () => {}) => Promise<void>): Promise<void>;
+}
+
+export default class extends EventEmitter {
+  private amqp: Amqp;
+
+  private client?: {
+    (...args: any[]): Promise<any>;
+    client: Client;
+  };
+
+  public readonly initialized: Promise<void>;
+
+  constructor(
+    options?: {
+      amqp?: Amqp;
+      address?: string;
+    },
+  ) {
+    super();
+
+    this.amqp = options?.amqp || getAmqp();
+
+    this.initialized = this.start();
+  }
+
+  private async start() {
+    const client = await this.amqp.createClient('EventStore', {
+      deserialize: false,
+      serialize: false,
+    });
+
+    this.client = client;
+
+    const backoff = Backoff.fibonacci({
+      initialDelay: 100,
+      maxDelay: 10000,
+      randomisationFactor: 0,
+    });
+
+    const checkServer = async () => {
+      return Promise.race([
+        client({ type: 'Ping' }).then(() => true),
+        delay('1s').then(() => false),
+      ]);
+    };
+
+    await new Promise(async (resolve) => {
+      if (!(await checkServer())) {
+        backoff.on('ready', async () => {
+          if (await checkServer()) {
+            return resolve();
+          }
+
+          backoff.backoff();
+
+          return null;
+        });
+
+        backoff.backoff();
+      } else {
+        resolve();
+      }
+    });
+  }
 }
