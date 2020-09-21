1e52741c67a69d2a30199241fea7184b6a65aecb
rogermadjos <roger@hov.co>
2020-09-22 07:31:46 +0800

    feat(error): update dependencies

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 export default class AppError<T = Record<string, any>> extends Error {
-  public readonly service?: string;
+  public readonly service?: string = process.env.SERVICE || process.env.SERVICE_NAME;
 
   public readonly id: string = shortid.generate();
 
@@ -13,8 +13,6 @@ export default class AppError<T = Record<string, any>> extends Error {
     meta?: T,
   ) {
     super(message);
-
-    this.service = process.env.SERVICE || process.env.SERVICE_NAME;
 
     if (meta) {
       for (const [key, value] of Object.entries(meta)) {
