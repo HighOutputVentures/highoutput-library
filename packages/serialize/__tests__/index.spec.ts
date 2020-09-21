d587c654bd042ac5d2cea5db4ca7d4c0a154ea9f
rogermadjos <roger@hov.co>
2020-09-21 22:04:10 +0800

    feat: add serialize

---
+++ packages/serialize/__tests__/index.spec.ts
@@ -0,0 +1,102 @@
+import 'mocha';
+import { expect } from 'chai';
+import { serialize, deserialize } from '../src';
+
+const data: { deserialized: any, serialized: any }[] = [
+  {
+    deserialized: 1,
+    serialized: 1
+  },
+  {
+    deserialized: 'string',
+    serialized: 'string'
+  },
+  {
+    deserialized: { one: 1, two: 2 },
+    serialized: { one: 1, two: 2 }
+  },
+  {
+    deserialized: new Date('2020-09-20T04:00:00.000Z'),
+    serialized: {
+      __classObject: true,
+      type: 'Date',
+      data: '2020-09-20T04:00:00.000Z'
+    }
+  },
+  {
+    deserialized: new Set([1, 2, 3, 4]),
+    serialized: {
+      __classObject: true,
+      type: 'Set',
+      data: [1, 2, 3, 4]
+    }
+  },
+  {
+    deserialized: new Map<string, number>([['one', 1], ['two', 2], ['three', 3]]),
+    serialized: {
+      __classObject: true,
+      type: 'Map',
+      data: [['one', 1], ['two', 2], ['three', 3]]
+    }
+  },
+  {
+    deserialized: Buffer.from('mOfRn5t7rFI=', 'base64'),
+    serialized: {
+      __classObject: true,
+      type: 'Buffer',
+      data: 'mOfRn5t7rFI='
+    }
+  },
+  {
+    deserialized: {
+      number: 1,
+      string: 'string',
+      object: { one: 1, two: 2 },
+      date: new Date('2020-09-20T04:00:00.000Z'),
+      set: new Set([1, 2, 3, 4]),
+      map: new Map<string, number>([['one', 1], ['two', 2], ['three', 3]]),
+      buffer: Buffer.from('mOfRn5t7rFI=', 'base64')
+    },
+    serialized: {
+      number: 1,
+      string: 'string',
+      object: { one: 1, two: 2 },
+      date: {
+        __classObject: true,
+        type: 'Date',
+        data: '2020-09-20T04:00:00.000Z'
+      },
+      set: {
+        __classObject: true,
+        type: 'Set',
+        data: [1, 2, 3, 4]
+      },
+      map: {
+        __classObject: true,
+        type: 'Map',
+        data: [['one', 1], ['two', 2], ['three', 3]]
+      },
+      buffer: {
+        __classObject: true,
+        type: 'Buffer',
+        data: 'mOfRn5t7rFI='
+      }
+    }
+  }
+];
+
+describe('serialize', () => {
+  for (const { deserialized, serialized } of data) {
+    it('should return correct output', () => {
+      expect(serialize(deserialized)).to.deep.equal(serialized);
+    });
+  }
+});
+
+describe('deserialize', () => {
+  for (const { deserialized, serialized } of data) {
+    it('should return correct output', () => {
+      expect(deserialize(serialized)).to.deep.equal(deserialized);
+    });
+  }
+});
---
+++ packages/serialize/src/index.ts
@@ -0,0 +1,80 @@
+import R from 'ramda';
+
+export function serialize(object: any): any {
+  const type = typeof object;
+
+  if (type === 'object') {
+    if (object instanceof Date) {
+      return {
+        __classObject: true,
+        type: 'Date',
+        data: object.toISOString(),
+      };
+    }
+
+    if (object instanceof Set) {
+      return {
+        __classObject: true,
+        type: 'Set',
+        data: Array.from(object),
+      };
+    }
+
+    if (object instanceof Map) {
+      return {
+        __classObject: true,
+        type: 'Map',
+        data: Array.from(object),
+      };
+    }
+
+    if (object instanceof Buffer) {
+      return {
+        __classObject: true,
+        type: 'Buffer',
+        data: object.toString('base64'),
+      };
+    }
+
+    if (object === null) {
+      return null;
+    }
+
+    return R.map(serialize)(object);
+  }
+
+  return object;
+}
+
+export function deserialize(object: any): any {
+  const type = typeof object;
+
+  if (type === 'object'
+    && !(object instanceof Date || object instanceof Set || object instanceof Map || object instanceof Buffer)) {
+    if (object === null) {
+      return null;
+    }
+
+    if (object.__classObject) {
+      if (object.type === 'Date') {
+        return new Date(object.data);
+      }
+
+      if (object.type === 'Set') {
+        return new Set(object.data);
+      }
+
+      if (object.type === 'Map') {
+        return new Map(object.data);
+      }
+
+      if (object.type === 'Buffer') {
+        return Buffer.from(object.data, 'base64');
+      }
+    }
+
+    return R.map(deserialize)(object);
+  }
+
+  return object;
+}
