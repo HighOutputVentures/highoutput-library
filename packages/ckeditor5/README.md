767d448de098d94eed308a9754f53099d31b78b3
vmc08 <vmc08@yahoo.com>
2021-07-19 15:36:41 +0800

    feature(ckeditor5): add component based on ckeditor5 custom build

---
+++ packages/ckeditor5/README.md
@@ -0,0 +1,3 @@
+# HOV CKEditor 5
+
+Reusable editor component based on ckeditor5-custom-build library
---
+++ packages/ckeditor5/src/components/HOVEditor/HOVEditor.tsx
@@ -0,0 +1,99 @@
+import React, { FC, useMemo } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { ClassicEditor } from 'ckeditor5-custom-build';
+
+import { EditorTypes, HOVEditorProps } from '../../types/hov-editor';
+import {
+  getEmojiFeed,
+  getToolbars,
+  getPlugins,
+  getDisplayName,
+  moveCursorToEnd,
+  handleEscapeKey,
+  handleEnterKey,
+  emojiItemRenderer,
+  mentionItemRenderer,
+} from '../../utils/editorUtils';
+
+const HOVEditor: FC<HOVEditorProps> = (props) => {
+  const { editorType, value, onChange, placeholder, mentionables = [] } = props;
+
+  const editorToobars = useMemo(() => getToolbars(editorType), [editorType]);
+  const editorPlugins = useMemo(() => getPlugins(mentionables, editorType), [
+    mentionables,
+    editorType,
+  ]);
+
+  return (
+    <CKEditor
+      editor={ClassicEditor}
+      config={{
+        extraPlugins: editorPlugins,
+        ...(editorToobars.length && {
+          toolbar: {
+            items: editorToobars,
+          },
+        }),
+        mention: {
+          feeds: [
+            {
+              marker: '@',
+              feed: mentionables.map((m) => `@${getDisplayName(m)}`),
+              itemRenderer: (item: any) =>
+                mentionItemRenderer(item, mentionables),
+            },
+            {
+              marker: ':',
+              feed: getEmojiFeed(),
+              itemRenderer: emojiItemRenderer,
+            },
+          ],
+        },
+        ...(props.editorType === EditorTypes.CLASSIC && {
+          s3Upload: {
+            policyUrl: props.policy.url,
+            token: props.policy.token,
+          },
+        }),
+        placeholder,
+      }}
+      onReady={(editor: any) => {
+        if (editor) {
+          const command = editor.commands.get('mention');
+          command.on('execute', (_: any, data: any) => {
+            if (data[0].marker === ':') {
+              // run replacer function for emojis
+              const emoji = data[0].mention.emoji;
+              const currentMarkDownData = editor.getData();
+              const newData = currentMarkDownData.replaceAll(/:\S*:/gi, emoji);
+              editor.setData(newData);
+              moveCursorToEnd(editor);
+            }
+          });
+
+          if (props.editorType === EditorTypes.COMMENT) {
+            // should only be triggered when there's an initial value
+            // usually happens when editing
+            if (value) {
+              moveCursorToEnd(editor);
+              handleEscapeKey(editor, props.onEscape);
+            }
+            handleEnterKey(editor, props.onEnter);
+          } else {
+            onChange(editor.getData());
+          }
+          // setEditorInstance(editor);
+        }
+      }}
+      data={value}
+      onChange={(_: any, editor: any) => onChange(editor.getData())}
+    />
+  );
+};
+
+export default HOVEditor;
+export {
+  EditorTypes,
+  Mentionable,
+  HOVEditorProps,
+} from '../../types/hov-editor';
