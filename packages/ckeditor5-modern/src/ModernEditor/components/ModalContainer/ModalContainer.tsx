b9b305dcc4da2f004edd677140ffa3c96175e01f
vmc08 <vmc08@yahoo.com>
2021-07-21 18:20:29 +0800

    refactor(app): update modal component based on design

--- packages/ckeditor5-modern/src/ModernEditor/components/ModalContainer/ModalContainer.tsx
+++ packages/ckeditor5-modern/src/ModernEditor/components/ModalContainer/ModalContainer.tsx
@@ -2,23 +2,23 @@ import React, { FC, ReactNode } from 'react';
 import {
   Modal,
   ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
   ModalContent,
   ModalBody,
-  ModalCloseButton,
   useDisclosure,
   Box,
-  Divider,
 } from '@chakra-ui/react';
 
 interface ModalContainerProps {
   modalTrigger: ReactNode;
-  headerComponent: ReactNode;
+  title: string;
 }
 
 const ModalContainer: FC<ModalContainerProps> = ({
   children,
   modalTrigger,
-  headerComponent,
+  title,
 }) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   return (
@@ -26,14 +26,20 @@ const ModalContainer: FC<ModalContainerProps> = ({
       <Box w="100%" onClick={() => onOpen()}>
         {modalTrigger}
       </Box>
-      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+      <Modal isOpen={isOpen} onClose={onClose} size="2xl">
         <ModalOverlay />
-        <ModalContent p="4">
-          <Box>
-            {headerComponent}
-            <ModalCloseButton top="5" right="4" />
-          </Box>
-          <Divider colorScheme="blue" pt="4" mb="4" />
+        <ModalContent>
+          <ModalHeader
+            px="8"
+            py="6"
+            fontSize="2xl"
+            color="#A8A29E"
+            fontWeight="normal"
+            borderBottom="1px solid #E7E5E4"
+          >
+            {title}
+          </ModalHeader>
+          <ModalCloseButton right="22px" top="6" color="#A8A29E" />
           <ModalBody p="0">{children}</ModalBody>
         </ModalContent>
       </Modal>
