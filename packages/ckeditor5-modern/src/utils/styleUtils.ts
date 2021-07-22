14a6647d48eb9b97964647f54b15e4f7fdf2f9ea
vmc08 <vmc08@yahoo.com>
2021-07-22 11:30:58 +0800

    refactor(app): add editor style

---
+++ packages/ckeditor5-modern/src/utils/styleUtils.ts
@@ -0,0 +1,23 @@
+export const MODERN_EDITOR_STYLE = {
+  '.ck-toolbar': {
+    display: 'none',
+  },
+  '.ck-content': {
+    border: 0,
+    padding: '0 16px 0 32px',
+  },
+  '.ck-focused': {
+    border: '0 !important',
+    boxShadow: 'none !important',
+  },
+  '::-webkit-scrollbar': {
+    width: 2,
+  },
+  '::-webkit-scrollbar-track': {
+    borderRadius: 16,
+  },
+  '::-webkit-scrollbar-thumb': {
+    borderRadius: 16,
+    backgroundColor: '#A4A1AF',
+  },
+};
