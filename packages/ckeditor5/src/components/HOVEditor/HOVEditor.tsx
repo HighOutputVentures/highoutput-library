47ba4f691d3b2b6475c51c81a2330fbd8b3166cd
Niel Berongoy <niel_berongoy@yahoo.com>
2021-10-15 09:19:00 +0800

    revert hov editor import

--- packages/ckeditor5/src/components/HOVEditor/HOVEditor.tsx
+++ packages/ckeditor5/src/components/HOVEditor/HOVEditor.tsx
@@ -1,4 +1,6 @@
-import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
+import React, { FC, useMemo } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { ClassicEditor } from 'ckeditor5-custom-build';
 
 import { EditorTypes, HOVEditorProps } from '../../types/hov-editor';
 import {
@@ -29,97 +31,77 @@ const HOVEditor: FC<HOVEditorProps> = props => {
     editorType,
   ]);
 
-  const editorRef = useRef<any>();
-  const [editorLoaded, setEditorLoaded] = useState(false);
-  const [classicLoaded, setClassicLoaded] = useState(false);
-  const { CKEditor, ClassicEditor } = editorRef.current || {};
-
-  useEffect(() => {
-    import('@ckeditor/ckeditor5-react').then(ck => {
-      editorRef.current = {
-        ...editorRef.current,
-        CKEditor: ck.CKEditor,
-      };
-
-      setEditorLoaded(true);
-    });
-
-    import('ckeditor5-custom-build').then(ck => {
-      editorRef.current = {
-        ...editorRef.current,
-        ClassicEditor: ck.ClassicEditor,
-      };
-
-      setClassicLoaded(true);
-    });
-  }, []);
-
-  return editorLoaded && classicLoaded ? (
-    <CKEditor
-      editor={ClassicEditor}
-      disabled={disabled}
-      config={{
-        extraPlugins: editorPlugins,
-        ...(editorToobars.length && {
-          toolbar: {
-            items: editorToobars,
-          },
-        }),
-        mention: {
-          feeds: [
-            {
-              marker: '@',
-              feed: mentionables.map(m => `@${getDisplayName(m)}`),
-              itemRenderer: (item: any) =>
-                mentionItemRenderer(item, mentionables),
+  return (
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        disabled={disabled}
+        config={{
+          extraPlugins: editorPlugins,
+          ...(editorToobars.length && {
+            toolbar: {
+              items: editorToobars,
             },
-            {
-              marker: ':',
-              feed: getEmojiFeed(),
-              itemRenderer: emojiItemRenderer,
-            },
-          ],
-        },
-        ...(props.editorType === EditorTypes.CLASSIC && {
-          s3Upload: {
-            policyUrl: props.policy.url,
-            token: props.policy.token,
+          }),
+          mention: {
+            feeds: [
+              {
+                marker: '@',
+                feed: mentionables.map(m => `@${getDisplayName(m)}`),
+                itemRenderer: (item: any) =>
+                  mentionItemRenderer(item, mentionables),
+              },
+              {
+                marker: ':',
+                feed: getEmojiFeed(),
+                itemRenderer: emojiItemRenderer,
+              },
+            ],
           },
-        }),
-        placeholder,
-      }}
-      onReady={(editor: any) => {
-        if (editor) {
-          const command = editor.commands.get('mention');
-          command.on('execute', (_: any, data: any) => {
-            if (data[0].marker === ':') {
-              // run replacer function for emojis
-              const emoji = data[0].mention.emoji;
-              const currentMarkDownData = editor.getData();
-              const newData = currentMarkDownData.replaceAll(/:\S*:/gi, emoji);
-              editor.setData(newData);
-              moveCursorToEnd(editor);
-            }
-          });
+          ...(props.editorType === EditorTypes.CLASSIC && {
+            s3Upload: {
+              policyUrl: props.policy.url,
+              token: props.policy.token,
+            },
+          }),
+          placeholder,
+        }}
+        onReady={(editor: any) => {
+          if (editor) {
+            const command = editor.commands.get('mention');
+            command.on('execute', (_: any, data: any) => {
+              if (data[0].marker === ':') {
+                // run replacer function for emojis
+                const emoji = data[0].mention.emoji;
+                const currentMarkDownData = editor.getData();
+                const newData = currentMarkDownData.replaceAll(
+                  /:\S*:/gi,
+                  emoji
+                );
+                editor.setData(newData);
+                moveCursorToEnd(editor);
+              }
+            });
 
-          if (props.editorType === EditorTypes.COMMENT) {
-            // should only be triggered when there's an initial value
-            // usually happens when editing
-            if (value) {
-              moveCursorToEnd(editor);
-              handleEscapeKey(editor, props.onEscape);
+            if (props.editorType === EditorTypes.COMMENT) {
+              // should only be triggered when there's an initial value
+              // usually happens when editing
+              if (value) {
+                moveCursorToEnd(editor);
+                handleEscapeKey(editor, props.onEscape);
+              }
+              handleEnterKey(editor, props.onEnter);
+            } else {
+              onChange(editor.getData());
             }
-            handleEnterKey(editor, props.onEnter);
-          } else {
-            onChange(editor.getData());
+            // setEditorInstance(editor);
           }
-          // setEditorInstance(editor);
-        }
-      }}
-      data={value}
-      onChange={(_: any, editor: any) => onChange(editor.getData())}
-    />
-  ) : null;
+        }}
+        data={value}
+        onChange={(_: any, editor: any) => onChange(editor.getData())}
+      />
+    </>
+  );
 };
 
 export default HOVEditor;
