f93c83ea9da3d326b167c3126a23be0337d45786
Rey Aleonar <reyaleonar@gmail.com>
2022-02-09 10:56:21 +0800

    fix(logger): remove highoutput serialize lib and replicate deserialize method

--- packages/logger/src/lib/serialize.ts
+++ packages/logger/src/lib/serialize.ts
@@ -1,5 +1,49 @@
 import * as R from 'ramda';
 
+export function deserialize(object: any): any {
+  const type = typeof object;
+
+  if (
+    type === 'object' &&
+    !(
+      object instanceof Date ||
+      object instanceof Set ||
+      object instanceof Map ||
+      object instanceof Buffer
+    )
+  ) {
+    if (object === null) {
+      return null;
+    }
+
+    if (object instanceof Array) {
+      return object.map(deserialize);
+    }
+
+    if (object.__classObject) {
+      if (object.type === 'Date') {
+        return new Date(object.data);
+      }
+
+      if (object.type === 'Set') {
+        return new Set(object.data.map(deserialize));
+      }
+
+      if (object.type === 'Map') {
+        return new Map(object.data.map(deserialize));
+      }
+
+      if (object.type === 'Buffer') {
+        return Buffer.from(object.data, 'base64');
+      }
+    }
+
+    return R.map(deserialize)(object);
+  }
+
+  return object;
+}
+
 export const serialize = (data: any) => {
   const type = typeof data;
 
