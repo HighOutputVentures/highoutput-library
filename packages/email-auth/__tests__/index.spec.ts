c5c27cdf6dec573585e301dbdd834d1a274a47a6
marshalWilliam <marshalwilliam500@gmail.com>
2022-05-16 18:06:54 +0800

    feat(test): add fail tests

--- packages/email-auth/__tests__/index.spec.ts
+++ packages/email-auth/__tests__/index.spec.ts
@@ -1,6 +1,6 @@
 import { Chance } from 'chance';
+import cryptoRandomString from 'crypto-random-string';
 
-import { Email } from '../src/types';
 import { setup, SetupContext, teardown } from './setup';
 
 const chance = new Chance();
@@ -16,20 +16,59 @@ afterEach(async function (this: Context) {
 });
 
 describe('#sendEmail', () => {
-  it('should send email', async function (this: Context) {
-    const email = chance.email() as Email;
+  it('should fail to send email', async function (this: Context) {
+    const response = await this.request.post('/users/login').send({});
 
-    const response = await this.request.post('/users/login').send({ email });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe('Error Email Required');
+  });
+
+  it('should send email', async function (this: Context) {
+    const emailAdress = chance.email();
+
+    const response = await this.request.post('/users/login').send({ email: emailAdress });
 
     expect(response.status).toBe(200);
     expect(response.body).toStrictEqual({
-      text: `Email sent to ${email} \n\n <a href="/users/login/otp"> login </a>`,
+      text: `Email sent to ${emailAdress} \n\n <a href="/users/login/otp"> login </a>`,
       message: 'Email OTP',
     });
   });
 });
 
+describe('#get', () => {
+  test('initial get', async function (this: Context) {
+    const response = await this.request.get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeEmpty();
+  });
+});
+
 describe('#authenticate', () => {
+  it('should fail to authenticate: email and otp not provided', async function (this: Context) {
+    const response = await this.request
+      .post('/users/login/otp')
+      .send({});
+
+    expect(response.status).toBe(401);
+    expect(response.body).not.toHaveProperty('text');
+    expect(response.body.message).toBeUndefined();
+  });
+
+  it('should fail to authenticate: email and otp not found', async function (this: Context) {
+    const emailAdress = chance.email();
+    const OTP = cryptoRandomString({ length: 6, type: 'numeric' });
+
+    const response = await this.request
+      .post('/users/login/otp')
+      .send({ email: emailAdress, otp: OTP });
+
+    expect(response.status).toBe(401);
+    expect(response.body).not.toHaveProperty('text');
+    expect(response.body.message).toBeUndefined();
+  });
+
   it('should authenticate given email and otp', async function (this: Context) {
     const response = await this.request
       .post('/users/login/otp')
