d1abc77a1763ae83b82b42abf4e6d8700ddfd16b
marshalWilliam <marshalwilliam500@gmail.com>
2022-05-16 16:55:53 +0800

    feat: add jest.config file

---
+++ packages/email-auth/jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['jest-extended/all'],
+  moduleNameMapper: {
+    '^@libraries/(.*)$': '<rootDir>/libraries/$1',
+  },
+
+  testEnvironment: 'node',
+  maxWorkers: '50%',
+  testTimeout: 10_0000,
+  testMatch: ['**/**/*.spec.ts'],
+
+  bail: 1,
+  verbose: true,
+  watchman: false,
+
+  collectCoverageFrom: [
+    'libraries/**/!(error).ts',
+    'services/**/!(container).ts',
+    '!(services/api/modules/directives/**/*.ts)',
+  ],
+  coverageReporters: ['text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 70,
+      lines: 70,
+      statements: 80,
+    },
+  },
+};
