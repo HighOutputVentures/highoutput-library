f655d5fbf05637df8776379a4c2451be7677b9bc
marshalWilliam <marshalwilliam500@gmail.com>
2022-05-17 15:01:13 +0800

    refactor: move adapters to adapters folder

---
+++ packages/email-auth/src/adapters/send-grid-adapter.ts
@@ -0,0 +1,35 @@
+import sgMail from '@sendgrid/mail';
+import { EmailableProvider } from '../types';
+
+type SenderInfoType = {
+  name: string;
+  email: string;
+};
+
+export class SendGridAdapter implements EmailableProvider {
+  private readonly sgMail: typeof sgMail = sgMail;
+
+  private readonly senderInfo!: SenderInfoType;
+
+  constructor(params: {
+    sendGridApiKey: string;
+    from: SenderInfoType;
+  }) {
+    this.setApiKey(params.sendGridApiKey);
+    this.senderInfo = params.from;
+  }
+
+  setApiKey(key: string): void {
+    this.sgMail.setApiKey(key);
+  }
+
+  async sendEmail<MailDataRequired, ClientResponse>(message: MailDataRequired): Promise<ClientResponse> {
+    const msg = {
+      from: this.senderInfo,
+      ...message,
+    };
+    const [result] = await this.sgMail.send(msg as never);
+
+    return result as never;
+  }
+}
