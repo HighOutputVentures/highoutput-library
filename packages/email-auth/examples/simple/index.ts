b1281719a7bbbc3e6fd4b8f8c963437e79ab6c33
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-05-20 09:27:10 +0800

    feat: rename app.ts to index.ts

---
+++ packages/email-auth/examples/simple/index.ts
@@ -0,0 +1,42 @@
+import http from 'http';
+import mongoose from 'mongoose';
+
+import { EmailAuthentication } from '../../src';
+import { SendGridAdapter } from '../../src/adapters/send-grid-adapter';
+import { MongooseAdapter } from '../../src/adapters/mongoose-adapter';
+
+const dbURI = 'mongodb://localhost:27017/test-middleware';
+
+(async () => await mongoose.connect(dbURI).catch((e) => console.error(e)))();
+
+const persistenceAdapter = new MongooseAdapter({
+  db: mongoose.connection,
+  userCollectionString: 'users',
+})
+
+const EMAIL_PROVIDER_CONFIG = {
+  apiKey: process.env.SENDGRID_API_KEY as string,
+  from: {
+    email: process.env.SENDER_EMAIL as string || 'emailauth@hov.co',
+    name: process.env.SENDER_NAME as string || 'no-reply',
+  }
+}
+
+
+const emailProviderAdapter = new SendGridAdapter(EMAIL_PROVIDER_CONFIG);
+
+const server = http.createServer();
+
+const emailAuthentication = new EmailAuthentication({
+  server,
+
+  persistenceAdapter,
+
+  emailProviderAdapter,
+
+  jwtSecretKey: 'SECRET',
+});
+
+emailAuthentication.use();
+
+server.listen(8080);
