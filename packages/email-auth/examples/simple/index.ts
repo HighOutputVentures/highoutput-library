05db07b2e696d6be5075e3f695627fb97bfc6231
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2022-05-20 14:07:01 +0800

    refactor: refactor example

--- packages/email-auth/examples/simple/index.ts
+++ packages/email-auth/examples/simple/index.ts
@@ -10,10 +10,12 @@ const dbURI = 'mongodb://localhost:27017/test-middleware';
 
 (async () => await mongoose.connect(dbURI).catch((e) => console.error(e)))();
 
-const persistenceAdapter = new MongooseAdapter({
+const PERSISTENCE_CONFIG = {
   db: mongoose.connection,
   userCollectionString: 'users',
-})
+};
+
+const persistenceAdapter = new MongooseAdapter(PERSISTENCE_CONFIG);
 
 const EMAIL_PROVIDER_CONFIG = {
   apiKey: process.env.SENDGRID_API_KEY as string,
@@ -21,9 +23,7 @@ const EMAIL_PROVIDER_CONFIG = {
     email: process.env.SENDER_EMAIL as string || 'emailauth@hov.co',
     name: process.env.SENDER_NAME as string || 'no-reply',
   }
-}
-
-
+};
 const emailProviderAdapter = new SendGridAdapter(EMAIL_PROVIDER_CONFIG);
 
 const server = http.createServer();
