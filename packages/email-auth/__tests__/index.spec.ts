6b23bc505e9a76543ca30faab2524573bbdea251
marshalWilliam <marshalwilliam500@gmail.com>
2022-05-23 16:38:17 +0800

    feat: add test for default middleware response

--- packages/email-auth/__tests__/index.spec.ts
+++ packages/email-auth/__tests__/index.spec.ts
@@ -18,9 +18,9 @@ describe('EmailAuthentication', () => {
   describe('#sendEmail', () => {
     it('should throw an error when `to` is not supplied', async function (this: Context) {
       const response = await this.request.post('/generateOtp').send({
-        message: {}
+        message: {},
       });
-  
+
       expect(response.body).toStrictEqual({
         message: '`to` should be supplied',
       });
@@ -34,27 +34,35 @@ describe('EmailAuthentication', () => {
         .send({
           otp: '123456',
         });
-  
+
       expect(response.status).toBe(400);
       expect(response.body).toStrictEqual({
         message: '`email` should be provided',
       });
     });
-  
+
     it('should throw an error when `otp is empty', async function (this: Context) {
       const emailAdress = chance.email();
-  
+
       const response = await this.request
         .post('/validateOtp')
         .send({ email: emailAdress });
-  
+
       expect(response.status).toBe(400);
       expect(response.body).toStrictEqual({
         message: '`otp` should be provided',
       });
     });
   });
+
+  describe('#defaultResponse', () => {
+    it('should return default response', async function (this: Context) {
+      const response = await this.request.get('/');
+
+      expect(response.status).toBe(404);
+      expect(response.body).toStrictEqual({
+        message: 'not found',
+      });
+    });
+  });
 });
-
-
-
