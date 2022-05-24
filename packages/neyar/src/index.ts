05da3eb438683500c5cece8f58d6632f2a2c8c06
Niel Berongoy <niel_berongoy@yahoo.com>
2022-05-24 16:50:37 +0800

    seperated editor plugin and hov plugin

--- packages/neyar/src/index.ts
+++ packages/neyar/src/index.ts
@@ -1,8 +1,14 @@
 import EditorJS from '@editorjs/editorjs';
+
+/** Editor.js Plugin */
 import Header from '@editorjs/header';
 import Code from '@editorjs/code';
 import Paragraph from '@editorjs/paragraph';
 import CheckList from '@editorjs/checklist';
+
+export { EditorJS, Header, Code, Paragraph, CheckList };
+
+/** HOV Custom Plugin */
 import NeyarText from './plugins/NeyarText';
 
-export { EditorJS, Header, Code, Paragraph, NeyarText, CheckList };
+export { NeyarText };
