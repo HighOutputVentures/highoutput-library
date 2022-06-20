5cc2c4b3c023485ae34d5487eaa132a9ab76c050
rogermadjos <roger@hov.co>
2022-06-20 16:44:07 +0800

    fix(email-auth): fix typescript errors

--- packages/email-auth/src/adapters/mongoose-storage-adapter.ts
+++ packages/email-auth/src/adapters/mongoose-storage-adapter.ts
@@ -2,15 +2,15 @@ import { StorageAdapter } from '../interfaces';
 import { User } from '../lib/types';
 
 export class MongooseStorageAdapter implements StorageAdapter {
-  validateOtp(params: { otp: string; }): Promise<User | null> {
+  validateOtp(_params: { otp: string; }): Promise<User | null> {
     throw new Error('Method not implemented.');
   }
 
-  saveOtp(params: { user: Buffer; otp: string; }): Promise<void> {
+  saveOtp(_params: { user: Buffer; otp: string; }): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  findOneUserByEmail(params: { email: string; }): Promise<User | null> {
+  findOneUserByEmail(_params: { email: string; }): Promise<User | null> {
     throw new Error('Method not implemented.');
   }
 }
