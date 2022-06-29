b4308084d3f0f43c72eaf3bd955b25c2352e1adc
Alres Arena <alres.arena2019@gmail.com>
2022-06-29 11:46:30 +0800

    feat: revamp query options

--- packages/repository/src/repository.ts
+++ packages/repository/src/repository.ts
@@ -1,4 +1,4 @@
-import { Model, Document, FilterQuery, Connection, Schema, QueryOptions } from 'mongoose';
+import { Model, Document, FilterQuery, Connection, Schema } from 'mongoose';
 import R from 'ramda';
 import { ObjectId } from '@highoutput/object-id';
 import { BaseEntity } from './types';
@@ -166,7 +166,11 @@ export class Repository<
     return deserialize(document) as TEntity;
   }
 
-  async find(filter: FilterQuery<TEntity>, options?: QueryOptions): Promise<TEntity[]> {
+
+  async find(filter: FilterQuery<TEntity>, options?: Partial<{
+    sort: Record<string, 1 | -1> | undefined;
+    limit: number | undefined; 
+  }>): Promise<TEntity[]> {
     return R.map(
       (entity) => deserialize(entity),
       await this._model.find(serializeFilter(filter), null, options),
