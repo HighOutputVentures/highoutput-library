bf010340d7d096fd988724f3951386ce0af90d7c
Niel Berongoy <niel_berongoy@yahoo.com>
2022-07-05 21:57:08 +0800

    ui-components(fe): remove post install

--- packages/ui-components/example/index.tsx
+++ packages/ui-components/example/index.tsx
@@ -1,10 +1,10 @@
-import 'react-app-polyfill/ie11';
+import { ThemeProvider } from '@highoutput/ui-components';
 import * as React from 'react';
+import 'react-app-polyfill/ie11';
 import * as ReactDOM from 'react-dom';
-import { Provider } from '../.';
 
 const App = () => {
-  return <div></div>;
+  return <ThemeProvider></ThemeProvider>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
