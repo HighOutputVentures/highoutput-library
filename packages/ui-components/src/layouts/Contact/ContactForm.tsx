6d3c9d22812d40b04c0460fc0025e967d31786f8
Niel Berongoy <niel_berongoy@yahoo.com>
2022-07-10 19:58:18 +0800

    chore(fe): added validation in contact form ui components

--- packages/ui-components/src/layouts/Contact/ContactForm.tsx
+++ packages/ui-components/src/layouts/Contact/ContactForm.tsx
@@ -1,29 +1,60 @@
 import { Box, Button, Stack } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 import InputField from '../../components/InputField/InputField';
 import SelectField from '../../components/SelectField/SelectField';
 import TextAreaField from '../../components/TextareaField/TextareaField';
+import {
+  withContactFormSchema,
+  withContactFormSchemaValues,
+} from './validation';
 
 export interface ContactFormProps {}
 
 const ContactForm: FC<ContactFormProps> = () => {
+  const { register, handleSubmit, formState } = useForm<
+    withContactFormSchemaValues
+  >({
+    resolver: yupResolver(withContactFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      category: '',
+      description: '',
+    },
+    shouldUnregister: true,
+  });
+
+  const onSubmit = async (values: withContactFormSchemaValues) => {
+    console.log(values);
+  };
+
+  const { isSubmitting, errors } = formState;
+
   return (
-    <div>
-      <Box maxW={512}>
+    <Box maxW={512}>
+      <Box as="form" onSubmitCapture={handleSubmit(onSubmit)}>
         <Stack spacing={4}>
-          <InputField id="name" label="Name" placeholder="Input your name" />
           <InputField
+            {...register('name')}
+            id="name"
+            label="Name"
+            placeholder="Input your name"
+            errorMsg={errors.name?.message}
+            disabled={isSubmitting}
+          />
+          <InputField
+            {...register('email')}
             id="email"
             label="Email"
             placeholder="Input your email address"
-          />
-          <InputField
-            id="category"
-            label="Category"
-            placeholder="Select category"
+            errorMsg={errors.email?.message}
+            disabled={isSubmitting}
           />
           <SelectField
+            {...register('category')}
             id="category"
             label="Category"
             placeholder="Select category"
@@ -32,18 +63,28 @@ const ContactForm: FC<ContactFormProps> = () => {
               { label: 'General', value: 'general' },
               { label: 'How-to’s', value: 'howto' },
             ]}
+            errorMsg={errors.category?.message}
+            disabled={isSubmitting}
           />
           <TextAreaField
+            {...register('description')}
             id="description"
             label="Desciption of concern"
             placeholder="Enter description"
+            errorMsg={errors.description?.message}
+            disabled={isSubmitting}
           />
-          <Button w="full" variant="primary">
+          <Button
+            w="full"
+            variant="primary"
+            isLoading={isSubmitting}
+            type="submit"
+          >
             Send
           </Button>
         </Stack>
       </Box>
-    </div>
+    </Box>
   );
 };
 
