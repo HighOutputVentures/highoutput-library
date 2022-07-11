58de8bc3476c374b0f2251ce0caedf7e1789ff29
Alres Arena <alres.arena2019@gmail.com>
2022-07-11 09:08:55 +0800

    feat: export email auth server to index

--- packages/email-auth/src/index.ts
+++ packages/email-auth/src/index.ts
@@ -2,3 +2,4 @@ export * from './adapters';
 export * from './interfaces';
 export * from './types';
 export { EmailAuthentication } from './lib/email-authentication';
+export { EmailAuthServer } from './email-auth-server';
