2245e28bccd02edadc74fffe4a80c953d58e5f22
ccpacillos <clint.pacillos@gmail.com>
2022-07-14 08:31:14 +0800

    refactor: rename to Analytics

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,9 +1,10 @@
 import { ObjectId } from '@highoutput/object-id';
 import mixpanel, { Mixpanel } from 'mixpanel';
 
-export class Analytic {
+export class Analytics {
   protected project: string;
   protected driver: Mixpanel;
+
   constructor(params: { project: string }) {
     this.project = params.project;
     this.driver = mixpanel.init(process.env.MIXPANEL_TOKEN || 'secrets');
