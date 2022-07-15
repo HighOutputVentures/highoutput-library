0376eaec5fb4e8ac29515fbf7539b2ca97581e56
rogermadjos <roger@hov.co>
2022-07-15 10:11:45 +0800

    feat(pagination): rename retrievePage to retrieveConnection

--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -10,7 +10,7 @@ type Node = {
 
 export { convertFilter };
 
-export async function retrievePage<
+export async function retrieveConnection<
   TNode extends Node = Node,
   TDocument extends Node = Node,
 >(
@@ -45,7 +45,7 @@ export async function retrievePage<
 
   const limit = params.first || 1000;
 
-  const filter: FilterQuery<TDocument | null> = convertFilter(params.filter) || {};
+  const filter: FilterQuery<TDocument | null> = convertFilter(params.filter || {}) ;
 
   let totalCount = 0;
 
@@ -120,5 +120,7 @@ export async function retrievePage<
     },
   };
 }
+
+export const retrievePage = retrieveConnection;
 
 // publish trigger: 1
