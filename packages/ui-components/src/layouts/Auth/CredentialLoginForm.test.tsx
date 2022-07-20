5fb349146ba4898a335b9c84271bd96ddbe419c6
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-07-20 14:25:39 +0800

    resolved new changes

--- packages/ui-components/src/layouts/Auth/CredentialLoginForm.test.tsx
+++ packages/ui-components/src/layouts/Auth/CredentialLoginForm.test.tsx
@@ -1,45 +1,32 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import CredentialLoginForm from './CredentialLoginForm';
 
-describe('Login Form Component', () => {
-  it('should display error message on email', async () => {
+describe('Login Form Email-based Component', () => {
+  beforeEach(() => {
     render(<CredentialLoginForm />);
+  });
+  it('should display error message on email', async () => {
     const emailInput = screen.getByRole('input', { name: 'email-input' });
     const submitBtn = screen.getByRole('button', { name: 'Login' });
     fireEvent.change(emailInput, { target: { value: 'hello@g' } });
     fireEvent.click(submitBtn);
-    await waitFor(() =>
-      expect(
-        screen.getByText('Please enter a valid email address')
-      ).toBeInTheDocument()
+    const errorText = await screen.findByText(
+      /please enter a valid email address/i
     );
+    expect(errorText).toBeInTheDocument();
   });
-  it('should display error message on name', async () => {
-    render(
-      <CredentialLoginForm variant="name-password" nameLabel="Username" />
-    );
-    const nameInput = screen.getByRole('input', { name: 'name-input' });
-    const submitBtn = screen.getByRole('button', { name: 'Login' });
-    fireEvent.change(nameInput, { target: { value: null } });
-    fireEvent.click(submitBtn);
-    await waitFor(() =>
-      expect(screen.getByText('Name is required')).toBeInTheDocument()
-    );
-  });
+
   it('should display error message on password', async () => {
-    render(<CredentialLoginForm />);
     const passwordInput = screen.getByRole('input', { name: 'password-input' });
     const submitBtn = screen.getByRole('button', { name: 'Login' });
     fireEvent.change(passwordInput, { target: { value: null } });
     fireEvent.click(submitBtn);
-    await waitFor(() =>
-      expect(screen.getByText('Password is required')).toBeInTheDocument()
-    );
+    const errorText = await screen.findByText(/password is required/i);
+    expect(errorText).toBeInTheDocument();
   });
   it('should toggle showing and hiding password', () => {
-    render(<CredentialLoginForm />);
     const showHideBtn = screen.getByTestId('show-hide-btn');
     fireEvent.click(showHideBtn);
     expect(
@@ -51,19 +38,31 @@ describe('Login Form Component', () => {
     ).toBeInTheDocument();
   });
   it('should toggle login to signup form', async () => {
-    render(<CredentialLoginForm />);
     const switchLink = screen.getByTestId('switch-form-link');
     fireEvent.click(switchLink);
-    await waitFor(() =>
-      expect(
-        screen.getByRole('link', { name: 'login-link-label' })
-      ).toBeInTheDocument()
-    );
+    const textLoginLabel = screen.getByRole('link', {
+      name: 'login-link-label',
+    });
+    expect(textLoginLabel).toBeInTheDocument();
     fireEvent.click(switchLink);
-    await waitFor(() =>
-      expect(
-        screen.getByRole('link', { name: 'signup-link-label' })
-      ).toBeInTheDocument()
+    const textSignUpLabel = await screen.findByRole('link', {
+      name: 'signup-link-label',
+    });
+    expect(textSignUpLabel).toBeInTheDocument();
+  });
+});
+describe('Login form Username-based component', () => {
+  beforeEach(() => {
+    render(
+      <CredentialLoginForm variant="name-password" nameLabel="Username" />
     );
+  });
+  it('should display error message on name', async () => {
+    const nameInput = screen.getByRole('input', { name: 'name-input' });
+    const submitBtn = screen.getByRole('button', { name: 'Login' });
+    fireEvent.change(nameInput, { target: { value: null } });
+    fireEvent.click(submitBtn);
+    const errorText = await screen.findByText(/name is required/i);
+    expect(errorText).toBeInTheDocument();
   });
 });
