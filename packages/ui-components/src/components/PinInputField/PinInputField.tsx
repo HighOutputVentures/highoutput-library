9a9dba6cd8bde1cda4bc0da0019340c7d086d95a
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-07-21 22:47:57 +0800

    component pin input field

---
+++ packages/ui-components/src/components/PinInputField/PinInputField.tsx
@@ -0,0 +1,49 @@
+import {
+  PinInput,
+  PinInputField as ChakraPinInputField,
+  PinInputProps,
+  HStack,
+} from '@chakra-ui/react';
+import React from 'react';
+import { forwardRef } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+interface PinInputFieldProps extends UseFormRegisterReturn {
+  numberOfFields?: number;
+  chakraPinInputProps?: Omit<PinInputProps, 'children'>;
+  errorMsg: string | undefined;
+}
+
+const PinInputField = forwardRef<HTMLInputElement, PinInputFieldProps>(
+  props => {
+    const { numberOfFields = 6, onChange, name, chakraPinInputProps } = props;
+
+    return (
+      <HStack spacing={3}>
+        <PinInput
+          otp
+          type={chakraPinInputProps?.type}
+          errorBorderColor="red.300"
+          isInvalid={Boolean(props?.errorMsg)}
+          onChange={value => onChange?.({ target: { value, name } })}
+          {...chakraPinInputProps}
+        >
+          {Array.from({ length: numberOfFields }).map((_, idx) => (
+            <ChakraPinInputField
+              fontSize="lg"
+              fontWeight="semibold"
+              borderRadius="4px"
+              w="12"
+              h="12"
+              key={idx}
+            />
+          ))}
+        </PinInput>
+      </HStack>
+    );
+  }
+);
+
+PinInputField.displayName = 'PinInputField';
+
+export default PinInputField;
