1aee61b940d90e963179744bb318eff7d4aee376
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-07-21 22:48:38 +0800

    added otp validation

--- packages/ui-components/src/layouts/Auth/validation.ts
+++ packages/ui-components/src/layouts/Auth/validation.ts
@@ -34,3 +34,39 @@ export const withCredentialFormSchemaName: SchemaOf<CredentialFormInputNameProps
 export type withCredentialFormSchemaNameValues = InferType<
   typeof withCredentialFormSchemaName
 >;
+
+export const generateEmailOTPSchema: SchemaOf<{
+  emailAddress: string;
+}> = object().shape({
+  emailAddress: string()
+    .email('Please enter a valid email address.')
+    .required('Email is required.'),
+});
+
+export type GenerateEmailOTPSchemaValues = InferType<
+  typeof generateEmailOTPSchema
+>;
+export const authenticateNumberSchema: SchemaOf<{
+  otp: string;
+}> = object().shape({
+  otp: string()
+    .matches(/^[0-9]+$/g, 'Invalid OTP code')
+    .length(6, 'OTP code is incomplete.')
+    .required('OTP code is required.'),
+});
+
+export type AuthenticateNumberSchemaValues = InferType<
+  typeof authenticateNumberSchema
+>;
+export const authenticateAlphaNumericSchema: SchemaOf<{
+  otp: string;
+}> = object().shape({
+  otp: string()
+    .matches(/^[a-zA-Z0-9]+$/g, 'Invalid OTP code')
+    .length(6, 'OTP code is incomplete.')
+    .required('OTP code is required.'),
+});
+
+export type AuthenticateAlphaNumericSchemaValues = InferType<
+  typeof authenticateAlphaNumericSchema
+>;
