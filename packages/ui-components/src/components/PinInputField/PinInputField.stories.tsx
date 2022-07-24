3808aecbd8518679854853c6c8cb2564d99794b6
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-07-24 17:10:29 +0800

    created pin input field stories

---
+++ packages/ui-components/src/components/PinInputField/PinInputField.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import { ThemeProvider } from '../..';
+import PinInputField from './PinInputField';
+
+export default {
+  title: 'UI Components/Form/Pin Input Field',
+  component: PinInputField,
+} as ComponentMeta<typeof PinInputField>;
+
+const Template: ComponentStory<typeof PinInputField> = args => (
+  <ThemeProvider>
+    <PinInputField {...args} />
+  </ThemeProvider>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  ...Default.args,
+};
