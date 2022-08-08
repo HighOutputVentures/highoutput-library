487ee80e5ddda2a68a92c0ba235ff470b4efc94b
Niel Berongoy <niel_berongoy@yahoo.com>
2022-08-08 13:49:24 +0800

    chore(ui-components): refactor contact page props

--- packages/ui-components/src/layouts/Contact/ContactCard.tsx
+++ packages/ui-components/src/layouts/Contact/ContactCard.tsx
@@ -1,21 +1,20 @@
-import { Box, BoxProps, Center, Text } from '@chakra-ui/react';
+import { Box, BoxProps, Center, Text, TextProps } from '@chakra-ui/react';
 import React, { FC, ReactNode } from 'react';
 import ContactForm, { ContactFormProps } from './ContactForm';
 
+type WithoutChildren<T> = Omit<T, 'children'>;
 export interface ContactCardProps extends BoxProps {
   children?: ReactNode;
   title?: string;
-  contactFormProps?: ContactFormProps;
   url?: string;
+  partProps?: {
+    contactForm: WithoutChildren<ContactFormProps>;
+    text: WithoutChildren<TextProps>;
+  };
 }
 
 const ContactCard: FC<ContactCardProps> = props => {
-  const {
-    children,
-    title = 'Drop your message',
-    contactFormProps,
-    url,
-  } = props;
+  const { children, title = 'Drop your message', partProps, url } = props;
   return (
     <Box
       w={512}
@@ -23,15 +22,23 @@ const ContactCard: FC<ContactCardProps> = props => {
       borderRadius="8px"
       padding="56px"
       boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
-      data-testid="box.contactcard.container"
       {...props}
+      data-testid="box.contactcard.container"
     >
       <Center mb={8} data-testid="center.contactcard.titleposition">
-        <Text size="text-3xl" data-testid="text.contactcard.title">
+        <Text
+          size="text-3xl"
+          {...partProps?.text}
+          data-testid="text.contactcard.title"
+        >
           {title}
         </Text>
       </Center>
-      {children ? children : <ContactForm {...contactFormProps} url={url} />}
+      {children ? (
+        children
+      ) : (
+        <ContactForm {...partProps?.contactForm} url={url} />
+      )}
     </Box>
   );
 };
--- packages/ui-components/src/layouts/Contact/ContactForm.tsx
+++ packages/ui-components/src/layouts/Contact/ContactForm.tsx
@@ -11,8 +11,12 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import React, { FC } from 'react';
 import { useForm } from 'react-hook-form';
 
-import InputField from '../../components/InputField/InputField';
-import TextAreaField from '../../components/TextareaField/TextareaField';
+import InputField, {
+  InputFieldProps,
+} from '../../components/InputField/InputField';
+import TextAreaField, {
+  TextAreaFieldProps,
+} from '../../components/TextareaField/TextareaField';
 import useSupport from './useSupport';
 import {
   ContactFormInputProps,
@@ -20,26 +24,34 @@ import {
   withContactFormSchemaValues,
 } from './validation';
 
+type WithoutChildren<T> = Omit<T, 'children'>;
+
 export interface ContactFormProps {
-  buttonProps?: ButtonProps;
   onSubmit?(values: ContactFormInputProps): void;
   url?: string;
+  partProps?: Partial<{
+    input: WithoutChildren<InputFieldProps>;
+    textarea: WithoutChildren<TextAreaFieldProps>;
+    button: WithoutChildren<ButtonProps>;
+  }>;
 }
 
-const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
+const ContactForm: FC<ContactFormProps> = props => {
+  const { onSubmit, url, partProps } = props;
   const { postSupport, hasError, isSuccess, isLoading } = useSupport();
-  const { register, handleSubmit, formState, reset } =
-    useForm<withContactFormSchemaValues>({
-      resolver: yupResolver(withContactFormSchema),
-      defaultValues: {
-        emailAddress: '',
-        message: '',
-        details: {
-          name: '',
-        },
+  const { register, handleSubmit, formState, reset } = useForm<
+    withContactFormSchemaValues
+  >({
+    resolver: yupResolver(withContactFormSchema),
+    defaultValues: {
+      emailAddress: '',
+      message: '',
+      details: {
+        name: '',
       },
-      shouldUnregister: true,
-    });
+    },
+    shouldUnregister: true,
+  });
 
   const onSubmitForm = async (values: withContactFormSchemaValues) => {
     if (url) {
@@ -68,6 +80,7 @@ const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
       >
         <Stack spacing={4}>
           <InputField
+            {...partProps?.input}
             {...register('details.name')}
             id="name"
             label="Name"
@@ -76,6 +89,7 @@ const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
             disabled={isSubmitting}
           />
           <InputField
+            {...partProps?.input}
             {...register('emailAddress')}
             id="emailAddress"
             label="Email"
@@ -85,6 +99,7 @@ const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
             data-testid="input.contactform.email"
           />
           <TextAreaField
+            {...partProps?.textarea}
             {...register('message')}
             id="message"
             label="Desciption of concern"
@@ -97,9 +112,9 @@ const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
             w="full"
             variant="primary"
             isLoading={isSubmitting || isLoading}
+            {...partProps?.button}
             type="submit"
             data-testid="button.contactform.submit"
-            {...buttonProps}
           >
             Send
           </Button>
@@ -114,7 +129,7 @@ const ContactForm: FC<ContactFormProps> = ({ buttonProps, onSubmit, url }) => {
           {isSuccess && (
             <Alert status="success">
               <AlertIcon />
-              <AlertDescription>Message successfully sent !</AlertDescription>
+              <AlertDescription>Message successfully sent!</AlertDescription>
             </Alert>
           )}
         </Stack>
