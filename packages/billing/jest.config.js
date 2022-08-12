6e7e94a67837f1c145ba514c766df98d054c0981
realmags <kentbryanmaglinao9@gmail.com>
2022-08-12 15:48:56 +0800

    chore: add dependencies

---
+++ packages/billing/jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  bail: 1,
+  verbose: true,
+  maxWorkers: 1,
+  testTimeout: 5000000,
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  /* bazel copies files using symlinks */
+  /* jest doesn't like symlinks by default */
+  /* enable symlinks and disable watchman to use symlinks */
+  haste: {
+    enableSymlinks: true,
+  },
+  watchman: false,
+};
