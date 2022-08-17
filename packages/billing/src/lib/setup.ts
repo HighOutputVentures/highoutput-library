e2d3ecde82e60d3a406d254350ecf5c31586d3d7
realmags <kentbryanmaglinao9@gmail.com>
2022-08-17 10:09:32 +0800

    chore(billing): refactor setupCustomerPortal as a dedicated module

--- packages/billing/src/lib/setup.ts
+++ packages/billing/src/lib/setup.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import Stripe from 'stripe';
-import { PortalConfig } from '../types';
 
 if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('Secret key is required.');
@@ -12,40 +11,3 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 });
 
 export default stripe;
-
-export async function setupCustomerPortal(
-  config: PortalConfig,
-  products: Stripe.BillingPortal.ConfigurationCreateParams.Features.SubscriptionUpdate.Product[],
-) {
-  const configuration = await stripe.billingPortal.configurations.create({
-    business_profile: {
-      headline: config.businessProfile.headline,
-      privacy_policy_url: config.businessProfile.privacyPolicyUrl,
-      terms_of_service_url: config.businessProfile.termsOfServiceUrl,
-    },
-    features: {
-      customer_update: {
-        allowed_updates: ['email'],
-        enabled: true,
-      },
-      payment_method_update: {
-        enabled: true,
-      },
-      invoice_history: {
-        enabled: true,
-      },
-      subscription_cancel: {
-        enabled: true,
-        mode: 'at_period_end',
-      },
-      subscription_update: {
-        enabled: true,
-        default_allowed_updates: ['price'],
-        products,
-      },
-    },
-    default_return_url: config.returnUrl,
-  });
-
-  console.log(`Portal configuration created successfully: ${configuration.id}`);
-}
--- packages/billing/src/modules/init.ts
+++ packages/billing/src/modules/init.ts
@@ -2,9 +2,9 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-dynamic-require */
 import { Command } from 'commander';
-import * as stripe from '../lib/setup';
 import createProducts from '../lib/create-products';
 import readConfig from '../lib/read-config';
+import setupCustomerPortal from '../lib/setup-customer-portal';
 
 const init = new Command('init')
   .description('Initialize the Stripe environment')
@@ -15,7 +15,11 @@ const init = new Command('init')
 
       const products = await createProducts(configFile.tiers);
 
-      await stripe.setupCustomerPortal(configFile.customerPortal, products);
+      const portalId = await setupCustomerPortal(
+        configFile.customerPortal,
+        products,
+      );
+      console.log(`Customer portal configured successfully: ${portalId}`);
     } catch (error) {
       console.error(`\x1b[31m${(error as Error).message}\x1b[0m`);
     }
