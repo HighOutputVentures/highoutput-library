1b258177e8edb60100db2a9f65bbeb374d209a7b
realmags <kentbryanmaglinao9@gmail.com>
2022-08-17 17:11:58 +0800

    test: add unit test for setupCustomerPortal

--- packages/billing/__tests__/helpers.ts
+++ packages/billing/__tests__/helpers.ts
@@ -2,7 +2,7 @@
 import * as R from 'ramda';
 import { faker } from '@faker-js/faker';
 import Stripe from 'stripe';
-import { ProductsConfig } from '../src/types';
+import { PortalConfig, ProductsConfig } from '../src/types';
 
 export function generateProductTiers() {
   const max = faker.random.numeric(1, { bannedDigits: '0' });
@@ -30,4 +30,22 @@ export function generatePriceObject() {
     id: `price_${faker.random.alphaNumeric(14)}`,
     product: `prod_${faker.random.alphaNumeric(14)}`,
   } as Stripe.Price;
+}
+
+export function generateSubscriptionProduct() {
+  return {
+    prices: [`price_${faker.random.alphaNumeric(14)}`],
+    product: `prod_${faker.random.alphaNumeric(14)}`,
+  } as Stripe.BillingPortal.ConfigurationCreateParams.Features.SubscriptionUpdate.Product;
+}
+
+export function generatePortalConfig() {
+  return {
+    returnUrl: faker.internet.url(),
+    businessProfile: {
+      headline: faker.company.catchPhrase(),
+      privacyPolicyUrl: faker.internet.url(),
+      termsOfServiceUrl: faker.internet.url(),
+    },
+  } as PortalConfig;
 }
