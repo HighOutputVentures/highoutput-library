2df5f7885c86be14f87fa1d9cbe426de95e2cab4
realmags <kentbryanmaglinao9@gmail.com>
2022-08-24 11:31:11 +0800

    test(billing): refactor tests to include StorageAdapter

--- packages/billing/__tests__/fixture.ts
+++ packages/billing/__tests__/fixture.ts
@@ -2,18 +2,21 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
 import request, { SuperTest, Test } from 'supertest';
-import BillingServer from '../src/billing-server';
+import mongoose, { Connection } from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 export type Context = {
   request: SuperTest<Test>;
   app: Express;
   server: Server;
+  mongoose: Connection;
+  mongod: MongoMemoryServer;
 };
 
-export async function setup(billingServer: BillingServer) {
+export async function setup() {
+  const mongod = await MongoMemoryServer.create();
+  const connection = mongoose.createConnection(mongod.getUri());
   const app = express();
-
-  app.use(billingServer.expressMiddleware());
 
   const server = app.listen(0);
 
@@ -23,9 +26,13 @@ export async function setup(billingServer: BillingServer) {
     request: supertest,
     app,
     server,
+    mongoose: connection,
+    mongod,
   } as Context;
 }
 
 export async function teardown(ctx: Context) {
   ctx.server.close();
+  await ctx.mongoose.close();
+  await ctx.mongod.stop();
 }
--- packages/billing/__tests__/get-tiers.spec.ts
+++ packages/billing/__tests__/get-tiers.spec.ts
@@ -1,22 +1,40 @@
 /* eslint-disable import/extensions */
 import { faker } from '@faker-js/faker';
+import { Schema } from 'mongoose';
+import MongooseStorageAdapter from '../src/adapters/mongoose-storage-adapter';
 import BillingServer from '../src/billing-server';
 import { setup, teardown } from './fixture';
 
 describe('GET /tiers', () => {
   test('request is valid -> should return products[]', async () => {
+    const ctx = await setup();
     const authorizationAdapter = {
       authorize: jest.fn(async () =>
         Promise.resolve({ id: Buffer.from(faker.datatype.uuid()) }),
       ),
     };
+    ctx.mongoose.model(
+      'User',
+      new Schema({
+        emailAddress: {
+          type: String,
+          required: true,
+        },
+      }),
+    );
+    const storageAdapter = new MongooseStorageAdapter({
+      connection: ctx.mongoose,
+      userModel: 'User',
+    });
     const billingServer = new BillingServer({
       stripeSecretKey:
         'sk_test_51LWeDVGrNXva3DrphN3qGT3dnhh2bAoNZ7O80w4XpMEbBlMeLul10aMS7a41PXZHl8vOpcDI6JZ7KoNTSBFyV9r800kV6WzTLo',
       authorizationAdapter,
+      storageAdapter,
       config: './__tests__/test-config.json',
     });
-    const ctx = await setup(billingServer);
+
+    ctx.app.use(billingServer.expressMiddleware());
 
     await ctx.request
       .get('/tiers')
