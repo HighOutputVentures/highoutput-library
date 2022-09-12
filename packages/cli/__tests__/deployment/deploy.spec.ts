eab83e1424a4088e16bd7b3c51596a2506348d0b
Alres Arena <alres.arena2019@gmail.com>
2022-09-12 13:12:41 +0800

    feat: add docker file optional parameter

--- packages/cli/__tests__/deployment/deploy.spec.ts
+++ packages/cli/__tests__/deployment/deploy.spec.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import program from '../../src/program';
 import generateDeployment from '../helpers.next/generate-deployment';
-import { DeploymentStatus } from '../../types';
+import { DeploymentStatus } from '../../src/types';
 import nock from 'nock';
 import { API_BASE_URL } from '../../src/library/contants';
 import { DateTime } from 'luxon';
@@ -72,6 +72,40 @@ describe('deployment', () => {
         deployment.id.toString(),
         '--tag',
         'v0.2.1',
+      ]);
+
+      const outputs = spy.mock.calls[0][0].split('\n');
+
+      expect(outputs[0]).toBe(chalk.bold('DEPLOYMENT ID       NAME                STATUS              DATE UPDATED        '));
+      expect(outputs[1]).toContain(DeploymentStatus.Updating);
+      expect(outputs[1]).toContain(DateTime.fromJSDate(deployment.dateTimeUpdated).toRelative() as string);
+    });
+
+    test('deploy the deployment with dockerfile argument', async () => {
+      const deployment = {
+        ...generateDeployment(),
+        dateTimeUpdated: DateTime.now().minus({minutes: chance.minute() }).toJSDate(),
+      }
+
+      nock(API_BASE_URL)
+        .post(`/deployments/${deployment.id.toString()}:deploy`)
+        .reply(200, {
+          ...deployment,
+          id: deployment.id.toString(),
+          status: DeploymentStatus.Updating,
+        }
+      );
+
+      const spy = jest.spyOn(global.console, 'log');
+
+      await program.parseAsync([
+        'node',
+        './dist/index.js',
+        'deployment',
+        'deploy',
+        deployment.id.toString(),
+        '--docker-file',
+         chance.word()
       ]);
 
       const outputs = spy.mock.calls[0][0].split('\n');
--- packages/cli/src/modules/deployment/deploy.ts
+++ packages/cli/src/modules/deployment/deploy.ts
@@ -1,5 +1,5 @@
-import { Deployment, ObjectID, ResourceType } from '../../types'
 import { Command } from 'commander';
+import { Deployment, ObjectID, ResourceType } from '../../types';
 import renderDeployments from '../../library/render-deployments';
 import * as client from '../../library/client';
 
@@ -10,21 +10,23 @@ command
   .description('Rebuild and deploy latest changes of your app.')
   .option('-b, --branch <branch_name>', 'Git branch name of code to deploy.')
   .option('-gt, --tag <tag_name>', 'Git tag name of code to deploy.')
+  .option('-df, --docker-file <file_name>', 'Custom Dockerfile to be used for building deployments.')
   .action(async (id: string, options: {
     branch?: string;
     tag?: string;
+    dockerFile?: string;
   }) => {
-  
     const result = await client.deploy({
       type: ResourceType.Deployment,
       id: ObjectID.from(id),
       body: {
         branch: options.branch,
         tag: options.tag,
+        dockerFile: options.dockerFile,
       },
     });
-    
+
     console.log(renderDeployments([result as Deployment]));
   });
 
-  export default command;
+export default command;
