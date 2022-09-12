5ed91fbf509962bc5dc4f5c18f202d0d0f27f040
realmags <kentbryanmaglinao9@gmail.com>
2022-09-12 10:21:04 +0800

    test(billing): add unit test for ApiProvider#postWebhook

--- packages/billing-next/src/providers/api.provider.test.ts
+++ packages/billing-next/src/providers/api.provider.test.ts
@@ -1,4 +1,5 @@
 import { Container } from 'inversify';
+import Stripe from 'stripe';
 import { generateFakeConfig } from '../../__tests__/helpers/generate-fake-config';
 import { IApiProvider } from '../interfaces/api.provider';
 import { ApiProvider } from './api.provider';
@@ -499,6 +500,377 @@ describe('ApiProvider', () => {
                 payment_status: expect.any(String),
               }),
             },
+          }),
+        );
+      },
+    );
+  });
+
+  describe('#postWebhook', () => {
+    test.concurrent(
+      'request is not Stripe verified -> should throw',
+      async () => {
+        const config = generateFakeConfig();
+
+        const container = new Container();
+
+        const StripeMock = {
+          webhooks: {
+            constructEvent: jest.fn(() => null),
+          },
+        };
+
+        container.bind(TYPES.Stripe).toConstantValue(StripeMock);
+        container.bind(TYPES.ConfigProvider).toConstantValue({
+          config,
+        });
+        container.bind(TYPES.StripeProviderStorageAdapter).toConstantValue({});
+        container.bind(TYPES.ApiProvider).to(ExpressApiProvider);
+
+        const provider = container.get<IApiProvider>(TYPES.ApiProvider);
+
+        try {
+          await provider.postWebhook({
+            body: {
+              rawBody: '',
+              signature: '',
+            },
+          });
+        } catch (error) {
+          expect((error as Error).message).toEqual(
+            'Cannot verify payload without signing secret.',
+          );
+        }
+      },
+    );
+
+    test.concurrent('unhandled event -> should throw', async () => {
+      const config = generateFakeConfig();
+
+      const container = new Container();
+      const stripe = new Stripe('', {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        apiVersion: null,
+      });
+
+      const webhookSecret = 'whsec_T1sWT0N2rHkaFNG8EDgJfryNdlg6r2MW';
+      const payloadString = JSON.stringify({}, null, 2);
+      const header = stripe.webhooks.generateTestHeaderString({
+        payload: payloadString,
+        secret: webhookSecret,
+      });
+
+      const StripeMock = {
+        webhooks: {
+          constructEvent: jest.fn(() => ({
+            type: 'UNHANDLED_EVENT',
+          })),
+        },
+      };
+
+      container.bind(TYPES.Stripe).toConstantValue(StripeMock);
+      container.bind(TYPES.ConfigProvider).toConstantValue({
+        config,
+      });
+      container.bind(TYPES.StripeProviderStorageAdapter).toConstantValue({});
+      container.bind(TYPES.ApiProvider).to(ExpressApiProvider);
+
+      const provider = container.get<IApiProvider>(TYPES.ApiProvider);
+
+      try {
+        await provider.postWebhook({
+          body: {
+            endpointSecret: webhookSecret,
+            rawBody: payloadString,
+            signature: header,
+          },
+        });
+      } catch (error) {
+        expect((error as Error).message).toMatch(/Unhandled event type/);
+      }
+    });
+
+    test.concurrent(
+      'customer.subscription.created and payment succeeded -> event is handled',
+      async () => {
+        const config = generateFakeConfig();
+
+        const container = new Container();
+
+        const customer = {
+          id: generateFakeId(IdType.USER),
+          stripeCustomer: generateFakeId(IdType.CUSTOMER),
+        };
+        const tier = {
+          id: 'starter',
+          stripeProduct: generateFakeId(IdType.PRODUCT),
+        };
+        const subscription = {
+          id: generateFakeId(IdType.SUBSCRIPTION),
+          customer: customer.stripeCustomer,
+          items: {
+            data: [
+              {
+                quantity: 1,
+                price: {
+                  product: {
+                    id: tier.stripeProduct,
+                  },
+                },
+              },
+            ],
+          },
+          latest_invoice: {
+            payment_intent: {
+              status: 'succeeded',
+            },
+          },
+        };
+
+        const StripeMock = {
+          webhooks: {
+            constructEvent: jest.fn(() => ({
+              type: 'customer.subscription.created',
+              data: {
+                object: subscription,
+              },
+            })),
+          },
+          subscriptions: {
+            retrieve: jest.fn(async () => Promise.resolve(subscription)),
+          },
+        };
+
+        const StripeProviderStorageAdapterMock = {
+          findCustomer: jest.fn(async () => Promise.resolve(customer)),
+          findTier: jest.fn(async () => Promise.resolve(tier)),
+          insertSubscription: jest.fn(async () => Promise.resolve()),
+        };
+
+        container.bind(TYPES.Stripe).toConstantValue(StripeMock);
+        container.bind(TYPES.ConfigProvider).toConstantValue({
+          config,
+        });
+        container
+          .bind(TYPES.StripeProviderStorageAdapter)
+          .toConstantValue(StripeProviderStorageAdapterMock);
+        container.bind(TYPES.ApiProvider).to(ExpressApiProvider);
+
+        const provider = container.get<IApiProvider>(TYPES.ApiProvider);
+
+        await provider.postWebhook({
+          body: {
+            endpointSecret: '',
+            rawBody: Buffer.from(''),
+            signature: '',
+          },
+        });
+
+        expect(StripeMock.webhooks.constructEvent).toBeCalledWith(
+          expect.any(Buffer),
+          expect.any(String),
+          expect.any(String),
+        );
+        expect(StripeMock.subscriptions.retrieve).toBeCalled();
+        expect(StripeProviderStorageAdapterMock.findCustomer).toBeCalled();
+        expect(StripeProviderStorageAdapterMock.findTier).toBeCalled();
+        expect(
+          StripeProviderStorageAdapterMock.insertSubscription,
+        ).toBeCalledWith(
+          expect.objectContaining({
+            id: expect.any(String),
+            stripeSubscription: expect.any(String),
+            tier: expect.any(String),
+            quantity: expect.any(Number),
+          }),
+        );
+      },
+    );
+
+    test.concurrent(
+      'customer.subscription.updated and payment succeeded-> event is handled',
+      async () => {
+        const config = generateFakeConfig();
+
+        const container = new Container();
+
+        const customer = {
+          id: generateFakeId(IdType.USER),
+          stripeCustomer: generateFakeId(IdType.CUSTOMER),
+        };
+        const tier = {
+          id: 'starter',
+          stripeProduct: generateFakeId(IdType.PRODUCT),
+        };
+        const subscription = {
+          id: generateFakeId(IdType.SUBSCRIPTION),
+          customer: customer.stripeCustomer,
+          items: {
+            data: [
+              {
+                quantity: 1,
+                price: {
+                  product: {
+                    id: tier.stripeProduct,
+                  },
+                },
+              },
+            ],
+          },
+          latest_invoice: {
+            payment_intent: {
+              status: 'succeeded',
+            },
+          },
+        };
+
+        const StripeMock = {
+          webhooks: {
+            constructEvent: jest.fn(() => ({
+              type: 'customer.subscription.updated',
+              data: {
+                object: subscription,
+              },
+            })),
+          },
+          subscriptions: {
+            retrieve: jest.fn(async () => Promise.resolve(subscription)),
+          },
+        };
+
+        const StripeProviderStorageAdapterMock = {
+          findCustomer: jest.fn(async () => Promise.resolve(customer)),
+          findTier: jest.fn(async () => Promise.resolve(tier)),
+          updateSubscription: jest.fn(async () => Promise.resolve()),
+        };
+
+        container.bind(TYPES.Stripe).toConstantValue(StripeMock);
+        container.bind(TYPES.ConfigProvider).toConstantValue({
+          config,
+        });
+        container
+          .bind(TYPES.StripeProviderStorageAdapter)
+          .toConstantValue(StripeProviderStorageAdapterMock);
+        container.bind(TYPES.ApiProvider).to(ExpressApiProvider);
+
+        const provider = container.get<IApiProvider>(TYPES.ApiProvider);
+
+        await provider.postWebhook({
+          body: {
+            endpointSecret: '',
+            rawBody: Buffer.from(''),
+            signature: '',
+          },
+        });
+
+        expect(StripeMock.webhooks.constructEvent).toBeCalledWith(
+          expect.any(Buffer),
+          expect.any(String),
+          expect.any(String),
+        );
+        expect(StripeMock.subscriptions.retrieve).toBeCalled();
+        expect(StripeProviderStorageAdapterMock.findCustomer).toBeCalled();
+        expect(StripeProviderStorageAdapterMock.findTier).toBeCalled();
+        expect(
+          StripeProviderStorageAdapterMock.updateSubscription,
+        ).toBeCalledWith(
+          expect.any(String),
+          expect.objectContaining({
+            stripeSubscription: expect.any(String),
+            tier: expect.any(String),
+            quantity: expect.any(Number),
+          }),
+        );
+      },
+    );
+
+    test.concurrent(
+      'customer.subscription.deleted -> event is handled',
+      async () => {
+        const config = generateFakeConfig();
+
+        const container = new Container();
+
+        const customer = {
+          id: generateFakeId(IdType.USER),
+          stripeCustomer: generateFakeId(IdType.CUSTOMER),
+        };
+        const tier = {
+          id: 'starter',
+          stripeProduct: generateFakeId(IdType.PRODUCT),
+        };
+        const subscription = {
+          id: generateFakeId(IdType.SUBSCRIPTION),
+          customer: customer.stripeCustomer,
+          items: {
+            data: [
+              {
+                quantity: 1,
+                price: {
+                  product: {
+                    id: tier.stripeProduct,
+                  },
+                },
+              },
+            ],
+          },
+          latest_invoice: {
+            payment_intent: {
+              status: 'succeeded',
+            },
+          },
+        };
+
+        const StripeMock = {
+          webhooks: {
+            constructEvent: jest.fn(() => ({
+              type: 'customer.subscription.deleted',
+              data: {
+                object: subscription,
+              },
+            })),
+          },
+        };
+
+        const StripeProviderStorageAdapterMock = {
+          findCustomer: jest.fn(async () => Promise.resolve(customer)),
+          updateSubscription: jest.fn(async () => Promise.resolve()),
+        };
+
+        container.bind(TYPES.Stripe).toConstantValue(StripeMock);
+        container.bind(TYPES.ConfigProvider).toConstantValue({
+          config,
+        });
+        container
+          .bind(TYPES.StripeProviderStorageAdapter)
+          .toConstantValue(StripeProviderStorageAdapterMock);
+        container.bind(TYPES.ApiProvider).to(ExpressApiProvider);
+
+        const provider = container.get<IApiProvider>(TYPES.ApiProvider);
+
+        await provider.postWebhook({
+          body: {
+            endpointSecret: '',
+            rawBody: Buffer.from(''),
+            signature: '',
+          },
+        });
+
+        expect(StripeMock.webhooks.constructEvent).toBeCalledWith(
+          expect.any(Buffer),
+          expect.any(String),
+          expect.any(String),
+        );
+        expect(StripeProviderStorageAdapterMock.findCustomer).toBeCalled();
+        expect(
+          StripeProviderStorageAdapterMock.updateSubscription,
+        ).toBeCalledWith(
+          expect.any(String),
+          expect.objectContaining({
+            stripeSubscription: expect.any(String),
+            tier: undefined,
+            quantity: undefined,
           }),
         );
       },
