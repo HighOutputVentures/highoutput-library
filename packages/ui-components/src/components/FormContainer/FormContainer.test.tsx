c3b9daae64b4c000af9572b93dbc15c5f0c98834
Glevinzon Dapal <glevinzond@gmail.com>
2022-09-14 12:00:38 +0800

    chore: use useId hook for data testid attribute for form container

--- packages/ui-components/src/components/FormContainer/FormContainer.test.tsx
+++ packages/ui-components/src/components/FormContainer/FormContainer.test.tsx
@@ -1,3 +1,5 @@
+import '@testing-library/react/dont-cleanup-after-each';
+
 import { Input } from '@chakra-ui/react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
@@ -6,7 +8,7 @@ import React from 'react';
 import FormContainer from './FormContainer';
 
 describe('Form Container Component', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     render(
       <FormContainer
         id="test"
@@ -19,19 +21,19 @@ describe('Form Container Component', () => {
 
   it('should renders form container form control', async () => {
     const formControl = await screen.findAllByTestId(
-      'formcontainer.formcontrol'
+      ':r0:-form-container-formcontrol'
     );
     expect(formControl).toHaveLength(1);
   });
 
   it('should renders form container form label', async () => {
-    const formLabel = await screen.findAllByTestId('formcontainer.label');
+    const formLabel = await screen.findAllByTestId(':r0:-form-container-label');
     expect(formLabel).toHaveLength(1);
   });
 
   it('should renders form container form error message', async () => {
     const formErrorMessage = await screen.findAllByTestId(
-      'formcontainer.error'
+      ':r0:-form-container-error'
     );
     expect(formErrorMessage).toHaveLength(1);
   });
